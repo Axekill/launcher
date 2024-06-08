a494a320a46772ed958b0f341bb449931f283d8e
Andrey <andrei3.ru@ya.ru>
2024-06-08 13:04:25 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module zone.gamers.launcher {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens zone.gamers.launcher to javafx.fxml;
+    exports zone.gamers.launcher;
+}
