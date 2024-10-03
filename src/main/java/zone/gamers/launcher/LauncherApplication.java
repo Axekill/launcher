d67cf5315a3a4d0995b0d6d47e685b83d40c72df
ГусевАЮ <andrei3.ru@ya.ru>
2024-10-03 16:37:11 +0300

    убрал выбор версий игры, добавил лейбл

--- src/main/java/zone/gamers/launcher/LauncherApplication.java
+++ src/main/java/zone/gamers/launcher/LauncherApplication.java
@@ -8,6 +8,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -21,18 +23,15 @@ import java.util.List;
 import java.util.Objects;
 
 public class LauncherApplication extends Application {
-    private ComboBox<String> versionComboBox;
     private ComboBox<String> serverComboBox;
-    private ListView<String> availableVersionsList;
+    private String availableVersionsGame;
     private ListView<String> availableServersList;
 
     @Override
     public void start(Stage stage) throws IOException {
         addIcon(stage);
 
-        versionComboBox = new ComboBox<>();
         serverComboBox = new ComboBox<>();
-        availableVersionsList = new ListView<>();
         availableServersList = new ListView<>();
 
         // Загружаем FXML
@@ -50,27 +49,31 @@ public class LauncherApplication extends Application {
         Button playButton = new Button("PLAY");
         configurePlayButton(playButton); // Метод для конфигурации кнопки
 
+        Label serverLabel = new Label("Сервер");
+
+
         root.getChildren().addAll(
-                new Label("версии игры:"), versionComboBox,
-                new Label("Серверы:"), serverComboBox,
+                serverLabel, serverComboBox,
                 playButton
         );
 
         stage.show();
-        loadAvailableVersions();
         loadAvailableServers();
-        configVersionCB(versionComboBox);
+        configServerLabel(serverLabel);
         configServerCB(serverComboBox);
+    }
+
+
+    private void configServerLabel(Label serverLabel) {
+        serverLabel.setLayoutX(100);
+        serverLabel.setLayoutY(680);
+        serverLabel.setFont(new Font(14.0));
+        serverLabel.setTextFill(Color.RED);
     }
 
     private void configServerCB(ComboBox<String> serverComboBox) {
         serverComboBox.setLayoutX(100);
         serverComboBox.setLayoutY(700);
-    }
-
-    private void configVersionCB(ComboBox<String> versionComboBox) {
-        versionComboBox.setLayoutX(200);
-        versionComboBox.setLayoutY(700);
     }
 
     // Метод для установки фона
@@ -89,38 +92,6 @@ public class LauncherApplication extends Application {
     }
 
 
-    private void loadAvailableVersions() {
-        try {
-            // Убедитесь, что URL правильный и сервер доступен
-            URL url = new URL("https://example.com/api/versions"); // Замените на URL вашего сервера
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            // Проверяем статус ответа
-            int responseCode = conn.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
-                List<String> versions = new ArrayList<>();
-
-                // Читаем данные из потока
-                while ((inputLine = in.readLine()) != null) {
-                    versions.add(inputLine.trim()); // Убираем пробелы для аккуратности
-                }
-
-                availableVersionsList.getItems().clear(); // Очистить старые данные перед добавлением новых
-                availableVersionsList.getItems().addAll(versions);
-                in.close();
-            } else {
-                showAlert("Ошибка: Сервер вернул статус " + responseCode);
-            }
-            conn.disconnect();
-
-        } catch (IOException e) {
-            showAlert("Ошибка при загрузке доступных версий: " + e.getMessage());
-            e.printStackTrace(); // Для отладки
-        }
-    }
 
     private void loadAvailableServers() {
         try {
@@ -155,14 +126,13 @@ public class LauncherApplication extends Application {
     }
 
     private void launchGame() {
-        String selectVersion = availableVersionsList.getSelectionModel().getSelectedItem();
         String selectServer = availableServersList.getSelectionModel().getSelectedItem();
 
-        if (selectVersion == null || selectServer == null) {
-            System.out.println("Пожалуйста выберите версию и сервер");
+        if (selectServer == null) {
+            System.out.println("Пожалуйста выберите сервер");
             return;
         }
-        String gamePath = "C:\\Program Files\\MyGame\\" + selectVersion + "\\game.exe"; // Путь к игре
+        String gamePath = "C:\\Program Files\\MyGame\\game.exe"; // Путь к игре
 
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(gamePath);
